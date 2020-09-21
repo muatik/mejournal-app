0a21d44f980df36f4d3a8cc45c172083653ce076
Mustafa Atik <muatik@gmail.com>
2020-09-21 15:45:56 +0200

    remove unused search form from the navbar

--- mejournal-ui/src/components/NavBar/NavBar.js
+++ mejournal-ui/src/components/NavBar/NavBar.js
@@ -21,23 +21,8 @@ const NavBar = ({ user, onLogout }) => {
     </div>
   );
 
-  const Form = () => (
-    <form className="form-inline">
-      <input
-        className="form-control mr-sm-2"
-        type="search"
-        placeholder="Search"
-        aria-label="Search"
-      />
-      <button className="btn btn-outline-success my-2 my-sm-0" type="submit">
-        Search
-      </button>
-    </form>
-  );
-
   const content = user && (
     <>
-      <Form />
       <Profile />
     </>
   );
